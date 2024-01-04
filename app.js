4eb144cc9621d59a7048deb9ffd3574797761a07
anushkaaa34 <133742135+anushkaaa34@users.noreply.github.com>
2024-01-04 23:53:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+
+const app = express();
+
+const dbURI = "mongodb+srv://netninja:8cvDLru%2E%40V6%40Er3@nodetuts.wvblapy.mongodb.net/";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => app.listen(3000))
+    .catch((err) => console.log(err));
+
+    // register view engine
+app.set('view engine' , 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use((req,res,next) => {
+    res.locals.path = req.path;
+    next();
+});
+
+app.get('/', (req,res) => {
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req,res) => {
+    res.render('about', {title:'About'});
+});
+
+app.use('/blogs', blogRoutes);
+
+app.use((req,res)=>{
+    res.status(404).render('404', {title: '404'});
+});
